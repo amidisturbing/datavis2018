301985a10cc734c4981e3bbef6b8f1a9a3660e4e
Alex <alexander.jaenisch@idealo.de>
2018-05-20 16:34:10 +0200

    merged my changes

--- data/script.R
+++ data/script.R
@@ -1,90 +1,102 @@
+#load the data
 setwd("datavis2018")
 lab <- read.csv2("data/LabMeasurements-Color-Card.csv")
 Master <- read.csv2("data/MasterColorCard.csv")
 
+#set plot matrix
+par(mfrow=c(2,2))
 #t() tranposes the matrix  and c() puts it all into one row
-MasterInOneRow <- c(t(as.matrix(Master[,9:11])))
-MasterInOneRow
+MasterAsOneRow <- c(t(as.matrix(Master[,9:11])))
+
 #creates a matrix by repeating the rows to fit the lab data 
-MasterRepeated <- matrix(rep(MasterInOneRow, 13*42), byrow = TRUE,nrow = 13*42)
-MasterRepeated
+MasterSameSizeAsLab <- matrix(rep(MasterAsOneRow, 13*42), byrow = TRUE,nrow = 13*42)
+
+#cuts off row and col column from lab data
 LabWithoutFirstColumns <- lab[,-c(1:2)]
 
-deltaMatrix <- MasterRepeated - LabWithoutFirstColumns
+#create a matrx that contains the differences between the Lab values of master and lab
+DeltaMasterLab <- MasterSameSizeAsLab - LabWithoutFirstColumns
 
-print(deltaMatrix)
-deltaMatrixPowerTwo <- deltaMatrix^2
+#square the differences
+DeltaMasterSquared <- DeltaMasterLab^2
 
-deltaMatrixPowerTwo
-dim(deltaMatrixPowerTwo)
-#deltEMatrix contains Lab Errors for each spot (one row is one colour card)
-deltEMatrix <- matrix(NA, nrow = 546, ncol = 64, byrow = FALSE)
-
-for (i in 1:ncol(deltEMatrix)) {
-  deltEMatrix[,i] <- sqrt(rowSums(deltaMatrixPowerTwo[,(i-1)*3+(1:3)]))
+#SpotErrorMatrix contains Lab Errors for each spot (one row is one colour card)
+SpotErrorMatrix <- matrix(NA, nrow = 546, ncol = 64, byrow = FALSE)
+#fill the empty matrix with the computed errors
+for (i in 1:ncol(SpotErrorMatrix)) {
+  SpotErrorMatrix[,i] <- sqrt(rowSums(DeltaMasterSquared[,(i-1)*3+(1:3)]))
 }
-
-deltaERangePerCard  <- matrix(NA, nrow = nrow(deltEMatrix), ncol = 2, byrow = FALSE)
-rownames(deltaERangePerCard) <- c(1:nrow(deltEMatrix))
+#compute min's and max's
+deltaERangePerCard  <- matrix(NA, nrow = nrow(SpotErrorMatrix), ncol = 2, byrow = FALSE)
+rownames(deltaERangePerCard) <- c(1:nrow(SpotErrorMatrix))
 colnames(deltaERangePerCard) <- c("Minimum","Maximum")
 #deltaERangePerCard <- as.table(deltaERangePerCard)
 #deltaERangePerCard["Minimum"] 
 
-for (i in 1:nrow(deltEMatrix)) {
-  rafMin <- min(deltEMatrix[i,])
-  rafMax <- max(deltEMatrix[i,])
-  minIndex <- match(c(rafMin),deltEMatrix[i,])
-  maxIndex <- match(c(rafMax),deltEMatrix[i,])
-  print(c(i, rafMin, minIndex, rafMax, maxIndex))
-  deltaERangePerCard[i,"Minimum"] <- min(deltEMatrix[i,])
-  deltaERangePerCard[i,"Maximum"] <- max(deltEMatrix[i,])
+for (i in 1:nrow(SpotErrorMatrix)) {
+  rafMin <- min(SpotErrorMatrix[i,])
+  rafMax <- max(SpotErrorMatrix[i,])
+  minIndex <- match(c(rafMin),SpotErrorMatrix[i,])
+  maxIndex <- match(c(rafMax),SpotErrorMatrix[i,])
+  #print(c(i, rafMin, minIndex, rafMax, maxIndex))
+  deltaERangePerCard[i,"Minimum"] <- min(SpotErrorMatrix[i,])
+  deltaERangePerCard[i,"Maximum"] <- max(SpotErrorMatrix[i,])
 }
 
+#count how many spots belong into each visibility group
+VisLevelOne <- sum(SpotErrorMatrix<=1)
+VisLevelTwo <- sum((SpotErrorMatrix>1) & (SpotErrorMatrix<2))
+VisLevelThree <- sum((SpotErrorMatrix>2) & (SpotErrorMatrix<10))
+VisLevelFour <- sum((SpotErrorMatrix>11) & (SpotErrorMatrix<49))
+VisLevelFive <- sum(SpotErrorMatrix>50)
 
-deltEMatrix
-which(deltEMatrix<=1, arr.ind=TRUE)
-countLevel1 <- sum(deltEMatrix<=1)
-countLevel1
-countLevel2 <- sum((deltEMatrix>1) & (deltEMatrix<2))
-countLevel2
-countLevel3 <- sum((deltEMatrix>2) & (deltEMatrix<10))
-countLevel3
-countLevel4 <- sum((deltEMatrix>11) & (deltEMatrix<49))
-countLevel4
-countLevel5 <- sum(deltEMatrix>50)
-countLevel5
-levelcount<-c(countLevel1,countLevel2,countLevel3,countLevel4,countLevel5)
-level <-c("Not Perceptable","Hardly Perceptable","At Glance","very Perceptible","Opposite")
-#visibility of color changes per spot
-barplot(levelcount,(1:5),names.arg = level)
+#plot the groups
+barplot(c(VisLevelOne,VisLevelTwo,VisLevelThree,VisLevelFour,VisLevelFive),
+        (1:5),
+        names.arg = c("Not Perceptable","Hardly Perceptable","At Glance","very Perceptible","Opposite"))
 
 #visibility of color changes per card
-meanErrorPerColourCard <- rowMeans(deltEMatrix)
-meanErrorPerColourCard
-#-----------------------------------------
-hist(meanErrorPerColourCard, breaks="Sturge", col="grey", labels = T,main="colour card errors")
-plot(density(meanErrorPerColourCard))
-#--------------------------------------------
+meanErrorPerTarget <- rowMeans(SpotErrorMatrix)
+
+#plot the groups
+hist(meanErrorPerTarget, breaks="Sturge", col="grey", labels = T,main="colour card errors")
+plot(density(meanErrorPerTarget))
+
 #visibility of color changes per sample
 meanErrorPerSample <- c(1:13)
 
 for (i in 1:13) {
   counter<-i;
   for (x in 1:42){
-    meanErrorPerSample[i]<-meanErrorPerSample[i]+meanErrorPerColourCard[counter]
+    meanErrorPerSample[i]<-meanErrorPerSample[i]+meanErrorPerTarget[counter]
     counter<-counter+13
   }
   meanErrorPerSample[i]<- meanErrorPerSample[i]/42
 }
-print(meanErrorPerSample)
 
 
-#z <- c("20 0 0", "50 0 0")
-#b <- do.call(rbind, lapply(strsplit(z, split = " "), as.numeric))
-#b <- LAB(b)
-#as(b, "RGB")
+#try to get back from error to lab values in table
+#get indexes of errors in certain level
+indexMatrix <- which(SpotErrorMatrix<=1, arr.ind=TRUE)
+#create helper to retrieve indexes of L values
+columnIndexesInLabMatrix<-seq(from=1, to=192,by=3)
+#create matrix to fill
+LabForLevel <- matrix(NA, nrow = nrow(indexMatrix), ncol = 3, byrow = FALSE)
 
+#get the lab values for each element of the index list
+for(i in 1:nrow(indexMatrix)){
+  LabForLevel[i,1]<-LabWithoutFirstColumns[indexMatrix[i,1],columnIndexesInLabMatrix[indexMatrix[i,2]]]
+  LabForLevel[i,2]<-LabWithoutFirstColumns[indexMatrix[i,1],columnIndexesInLabMatrix[indexMatrix[i,2]]+1]
+  LabForLevel[i,3]<-LabWithoutFirstColumns[indexMatrix[i,1],columnIndexesInLabMatrix[indexMatrix[i,2]]+2]
+}
 
+#convert lab matrx to rgb matrix
+RgbForLevel <- matrix(NA, nrow = nrow(LabForLevel), ncol = 3, byrow = FALSE)
+for(i in 1:nrow(LabForLevel)){
+  RgbForLevel[i,1:3]<-convertColor(LabForLevel[i,1:3],from = "Lab", to="sRGB")
+}
 
+library(scatterplot3d)
+scatterplot3d(RgbForLevel, color = rgb(RgbForLevel, alpha=1),xlab = "red", ylab = "green",zlab = "blue", main="Level 1")
 
 
